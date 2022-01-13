7bee4fce4aeec8f71a1a4b7ff09ebb0d2e3ae0ea
avolpe1998 <foxus20@gmail.com>
2022-01-13 19:28:25 +0100

    Aggiunto variabili al config.php

--- php/config.php
+++ php/config.php
@@ -4,3 +4,5 @@ $_SESSION['max_dim_img'] = 1300000; // Kb
 $_SESSION['$abs_path'] = $_SERVER["DOCUMENT_ROOT"].'/TecWeb2021/';
 $_SESSION['$abs_path_php'] = $_SERVER["DOCUMENT_ROOT"].'/TecWeb2021/php/';
 $_SESSION['$abs_path_img'] = $_SERVER["DOCUMENT_ROOT"].'/TecWeb2021/img/';
+$_SESSION['$abs_path_html'] = $_SERVER["DOCUMENT_ROOT"].'/TecWeb2021/html/';
+$_SESSION['$img_url'] = 'http://'.$_SERVER["SERVER_NAME"].'/TecWeb2021/img/';
