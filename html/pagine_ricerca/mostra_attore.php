bcfcd29edab4af482b92988fee9f57978eb0df29
aletz97 <alessiobertonmail@gmail.com>
2022-01-16 14:33:34 +0100

    nuova baseline. Aggiunta funzionalità di ricerca

--- html/pagine_ricerca/mostra_attore.php
+++ html/pagine_ricerca/mostra_attore.php
@@ -1,10 +1,16 @@
 <?php
-include_once '../../php/config.php';
+
+include '../../php/config.php';
 
 function getAbs_path(): void {
+    include_once($_SESSION['$abs_path_php']."logic/sessione.php");
     include_once($_SESSION['$abs_path_php']."logic/functions.php");
     include_once($_SESSION['$abs_path_php']."database/attore.php");
     include_once($_SESSION['$abs_path_php']."database/immagine.php");
+//    if ($_SESSION['logged'] == false) {
+//        header('location: ../accesso_negato.php');
+//        exit();
+//    }
     $_POST = array_map('empty_to_null', $_POST);
 }
 
@@ -15,19 +21,26 @@ $errore = '';
 $attore = new Attore();
 
 try {
-    $_SESSION["attore"] = $attore->find_nome_cognome_nascita("Brad", "Pitt", "1963-12-18");
+    $_SESSION["attore"] = $attore->find_nome_cognome_nascita("Ridley", "Scotto", "1973-12-18");
     if ($_SESSION["attore"] != null) {
-        print_r($_SESSION);
+        print_r($_SESSION["attore"]);
         $page = str_replace("#NOME#", $_SESSION["attore"]['Nome'], $page);
         $page = str_replace("#COGNOME#", $_SESSION["attore"]['Cognome'], $page);
         $page = str_replace("#DATA_NASCITA#", $_SESSION["attore"]['Data_nascita'], $page);
+        if ($_SESSION["attore"]['Data_morte'] != null) {
+            $page = str_replace("#DATA_MORTE_LABEL#", "Data morte: " , $page);
+            $page = str_replace("#DATA_MORTE#", $_SESSION["attore"]['Data_morte'], $page);
+        } else {
+            $page = str_replace("#DATA_MORTE_LABEL#", " " , $page);
+            $page = str_replace("#DATA_MORTE#", "", $page);
+        }
     } else {
         $errore = 'Attore bhoooooooooo ahah  ;:-((';
     }
     $page = str_replace("ERRORE", $errore, $page);
 } catch (Exception $e) {
     $pagina_errore = file_get_contents($_SESSION['$abs_path']."html/pagine_altre/errore.html");
-    $pagina_errore = str_replace("#ERROR_MESSAGE#", $e, $pagina_errore);
+    $pagina_errore = str_replace("</error_message>", $e, $pagina_errore);
     echo $pagina_errore;
 }
 
--- html/pagine_ricerca/mostra_film.php
+++ html/pagine_ricerca/mostra_film.php
@@ -1,77 +1,78 @@
 <?php
 include '../../php/config.php';
 
-function getAbs_path(): void {
-    include_once($_SESSION['$abs_path_php']."logic/functions.php");
-    include_once($_SESSION['$abs_path_php']."database/immagine.php");
-    include_once($_SESSION['$abs_path_php']."database/scheda_utente.php");
+function getAbs_path(): void
+{
+    include_once($_SESSION['$abs_path_php'] . "logic/functions.php");
+    include_once($_SESSION['$abs_path_php'] . "database/immagine.php");
+    include_once($_SESSION['$abs_path_php'] . "database/scheda_utente.php");
     $_POST = array_map('empty_to_null', $_POST);
 }
 
 getAbs_path();
 $page = file_get_contents("mostra_film.html");
 $piattaforma_section = "";
-$piattaforma_component = file_get_contents($_SESSION['$abs_path_html']."componenti/view_piattaforme.html");
+$piattaforma_component = file_get_contents($_SESSION['$abs_path_html'] . "componenti/view_piattaforme.html");
 $errore = '';
 $percorso_film = '';
 
 $immagine = new Immagine();
 
 if (isset($_GET["titolo"])) {
-    try {
-        if (isset($_SESSION["film"])) {
-            foreach ($_SESSION["film"] as $value) {
-                if ($_GET["titolo"] == $value["Titolo"]) {
-                    $id_immagine = $value["Locandina"];
-                    $page = str_replace("</titolo_pagina>", $value['Titolo'], $page);
-                    $page = str_replace("</titolo>", $value['Titolo'], $page);
-                    $page = str_replace("</lingua_titolo>", $value['Lingua_titolo'], $page);
-                    $page = str_replace("</anno>", $value['Anno'], $page);
-                    $page = str_replace("</paese>", $value['Paese'], $page);
-                    $page = str_replace("</durata>", secondsToTime($value['Durata']), $page);
-                    $percorso_immagine = $_SESSION['$img_url'].$immagine->find($id_immagine)["Percorso"];
+    if (isset($_SESSION["film"])) {
+        foreach ($_SESSION["film"] as $value) {
+            if ($_GET["titolo"] == $value["Titolo"]) {
+                $id_immagine = $value["Locandina"];
+                $page = str_replace("</titolo_pagina>", $value['Titolo'], $page);
+                $page = str_replace("</titolo>", $value['Titolo'], $page);
+                $page = str_replace("</lingua_titolo>", $value['Lingua_titolo'], $page);
+                $page = str_replace("</anno>", $value['Anno'], $page);
+                $page = str_replace("</paese>", $value['Paese'], $page);
+                $page = str_replace("</durata>", secondsToTime($value['Durata']), $page);
+                try {
+                    $percorso_immagine = $_SESSION['$img_url'] . $immagine->find($id_immagine)["Percorso"];
                     $descrizione_immagine = $immagine->find($id_immagine)["Descrizione"];
                     $page = str_replace("</url_img_film>", $percorso_immagine, $page);
                     $page = str_replace("</alt_img_film>", $descrizione_immagine, $page);
                     $page = str_replace("</error_message>", $errore, $page);
-                    
-                    foreach($value["Piattaforme"] as $piattaforma)
-                    {
+
+                    foreach ($value["Piattaforme"] as $piattaforma) {
                         $piattaforma_section .= $piattaforma_component;
                         $piattaforma_section = str_replace("</nome_piattaforma>", $piattaforma["Nome"], $piattaforma_section);
-                        $piattaforma_section = str_replace("</giorno_uscita>", 
-                                            !empty($piattaforma["Giorno_uscita"])? " disponibile fino a ".$piattaforma["Giorno_uscita"]:"", 
-                                            $piattaforma_section);
+                        $piattaforma_section = str_replace("</giorno_uscita>",
+                            !empty($piattaforma["Giorno_uscita"]) ? " disponibile fino a " . $piattaforma["Giorno_uscita"] : "",
+                            $piattaforma_section);
                         $piattaforma_section = str_replace("</cc>", $piattaforma["CC"] ? "CC" : "", $piattaforma_section);
                         $piattaforma_section = str_replace("</ad>", $piattaforma["AD"] ? "AD" : "", $piattaforma_section);
                     }
 
-                    $page .= $piattaforma_section;
-                    if($_SESSION["logged"]){
+                    $page.= $piattaforma_section;
+                    $_SESSION["logged"] = true;// TODO: togliere
+                    $_SESSION["username"] = 1;// TODO: togliere
+                    if ($_SESSION["logged"]) {
                         $scheda_utente = new SchedaUtente();
-                        $scheda_result = $scheda_utente->findByFilmUser($value["ID"],$_SESSION["username"]);
-                        if($scheda_result[0]["Visto"])                     
+                        $scheda_result = $scheda_utente->findByFilmUser($value["ID"], $_SESSION["username"]);
+                        if ($scheda_result[0]["Visto"])
                             $page = str_replace("</visto>", "VISTO", $page);
-                        if($scheda_result[0]["Salvato"])
+                        if ($scheda_result[0]["Salvato"])
                             $page = str_replace("</salvato>", "SALVATO", $page);
                     }
 
                     break;
+                } catch (Exception $e) {
+                    $pagina_errore = file_get_contents($_SESSION['$abs_path'] . "html/pagine_altre/errore.html");
+                    $pagina_errore = str_replace("</error_message>", "Errore server...", $pagina_errore);
+                    echo $pagina_errore;
                 }
             }
-        } else {
-            $pagina_errore = file_get_contents($_SESSION['$abs_path']."html/errore.html");
+        }
+    } else {
+            $pagina_errore = file_get_contents($_SESSION['$abs_path'] . "html/errore.html");
             $pagina_errore = str_replace("</error_message>", "Film non più esistente, very sorry", $pagina_errore);
             echo $pagina_errore;
-        }
-
-    } catch (Exception $e) {
-        $pagina_errore = file_get_contents($_SESSION['$abs_path']."html/pagine_altre/errore.html");
-        $pagina_errore = str_replace("</error_message>", "Errore server...", $pagina_errore);
-        echo $pagina_errore;
     }
 } else {
-    $pagina_errore = file_get_contents($_SESSION['$abs_path']."html/pagine_altre/errore.html");
+    $pagina_errore = file_get_contents($_SESSION['$abs_path'] . "html/pagine_altre/errore.html");
     $pagina_errore = str_replace("</error_message>", "Link non corretto lololol", $pagina_errore);
     echo $pagina_errore;
 }
