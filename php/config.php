a42c0c081683c1cec8d6dfd35555f7b6fa484d08
avolpe1998 <foxus20@gmail.com>
2022-01-24 11:24:23 +0100

    Tolto path assolutiti config.php

--- php/config.php
+++ php/config.php
@@ -4,6 +4,4 @@ session_start();
 if (!isset($_SESSION['logged'])) $_SESSION['logged'] = false;
 
 $_SESSION['max_dim_img'] = 1300000; // Kb
-$_SESSION['$img_url'] = 'http://'.$_SERVER["SERVER_NAME"].'/TecWeb2021/img/';
-$_SESSION['$img_not_found_url'] = 'http://'.$_SERVER["SERVER_NAME"].'/TecWeb2021/img/imgnotfound.jpg';
 ?>
