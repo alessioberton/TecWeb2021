60f88fd1a97275eb2d9d6b7efe5e6d34cb013119
not-saved <corradin.loris@outlook.com>
2022-01-29 12:11:04 +0100

    grafica searchbar

--- php/database/ricerca.php
+++ php/database/ricerca.php
@@ -7,12 +7,15 @@ require_once(__DIR__.'/../../php/database/immagine.php');
 
 $query = $_GET["q"];
 
-function suggestion($immagine_film, $descrizione_immagine_film, $titolo_film) {
+function suggestion($immagine_film, $descrizione_immagine_film, $titolo_film, $anno) {
     return <<<HTML
 		<li>
 			<a href="../pagine_ricerca/mostra_film.php?titolo=$titolo_film">
 				<img src="$immagine_film" alt='$descrizione_immagine_film'/>
-				$titolo_film
+				<div>
+					<h2>$titolo_film</h2>
+					<p>$anno</p>
+				</div>
 			</a>
 		</li>
 		HTML;
@@ -28,10 +31,12 @@ function crea_lista($titolo) {
             $film = new Film($lista_film[$i]);
             $info_img_film = $img_crud->find($film->locandina);
             $immagine_film = "../../img/".$info_img_film["Percorso"];
-            $html =  $html.suggestion($immagine_film, $info_img_film["Descrizione"], $film->titolo);
+            $html =  $html.suggestion($immagine_film, $info_img_film["Descrizione"], $film->titolo, $film->anno);
         }
 		$html = "<ul>" . $html . "</ul>";
-    }
+    } else {
+		$html = "<p>Nessun risultato trovato</p>";
+	}
     return $html;
 
 }
