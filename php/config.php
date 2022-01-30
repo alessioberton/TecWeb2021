47dac6c50c768ae9a90c1ac53986d5e908b3f501
Andrea <foxus20@gmail.com>
2022-01-30 09:37:58 +0100

    Tolto informazioni inutili inserisci_film.php

--- php/config.php
+++ php/config.php
@@ -3,7 +3,7 @@
 session_start();
 if (!isset($_SESSION['logged'])) $_SESSION['logged'] = false;
 
-$_SESSION['max_dim_img'] = 1300000; // Kb
+$_SESSION['max_dim_img'] = 1300000; // byte
 
 $_SESSION['sigle_paesi'] = array(
     'AF' => 'Afghanistan',
