c6e63e6816941059ca3c84e588905cdcb3a109f2
Andrea <foxus20@gmail.com>
2022-01-30 15:27:51 +0100

    Tolto lista film da sessione mostra_film.php

--- html/pagine_ricerca/mostra_film.php
+++ html/pagine_ricerca/mostra_film.php
@@ -8,9 +8,10 @@ function getAbs_path(): void
     require_once(__DIR__.'/../../php/database/immagine.php');
     require_once(__DIR__.'/../../php/database/scheda_utente.php');
     require_once(__DIR__.'/../../php/database/valutazione.php');
+    require_once(__DIR__.'/../../php/database/film_crud.php');
     require_once(__DIR__.'/../../html/componenti/header.php');
 
-    $_POST = array_map('empty_to_null', $_POST);
+    //$_POST = array_map('empty_to_null', $_POST);
 }
 
 getAbs_path();
@@ -25,10 +26,13 @@ $percorso_film = '';
 $immagine = new Immagine();
 $scheda_utente = new SchedaUtente();
 $valutazione = new Valutazione();
+$film_crud = new Film_crud();
+
+$lista_film = $film_crud->find_all();
 
 if (isset($_GET["titolo"])) {
-        if (isset($_SESSION["film"])) {
-            foreach ($_SESSION["film"] as $value) {
+        if (isset($lista_film)) {
+            foreach ($lista_film as $value) {
                 if ($_GET["titolo"] == $value["Titolo"]) {
                     $id_film = $value["ID"];
                     inserisciModificaValutazione($scheda_utente,$valutazione,$id_film);
@@ -80,7 +84,7 @@ if (isset($_GET["titolo"])) {
                                 $page = str_replace("#SELECTED_SALVATO#", "", $page);
                             }
 
-                            $valutazione_item = $valutazione->find($id_film,$_SESSION["user"]["Username"]);
+                            $valutazione_item = $valutazione->find($_SESSION["user"]["Username"],$id_film);
                             $page = empty($valutazione_item) ? str_replace("#MODIFICA_VALUTAZIONE#", "0", $page) : str_replace("#MODIFICA_VALUTAZIONE#", "1", $page);
                             $page = str_replace("#SELECTED".$valutazione_item["Stelle"]."#", "selected", $page);
                         }else{
@@ -120,7 +124,6 @@ echo $page;
 
 function inserisciModificaValutazione($scheda_utente, $valutazione, $id_film){
     global $errore;
-    global $_POST;
     
     if($_POST["valutazione_stelle"] && $_SESSION["logged"]){
         try{
