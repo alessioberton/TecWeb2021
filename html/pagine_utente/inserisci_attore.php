3bafdffe7fc74b341b5be4a9b5bd1440f8ee75e9
avolpe1998 <foxus20@gmail.com>
2022-01-31 10:10:58 +0100

    Aggiunto controlli via backend inserisci_attore

--- html/pagine_utente/inserisci_attore.php
+++ html/pagine_utente/inserisci_attore.php
@@ -23,37 +23,70 @@ if (!empty($_POST['nome']) && empty($_GET["inserted"])) {
     $descrizione_immagine = validate_input($_POST["descrizione_immagine"]);
     $immagine = $_FILES["immagine"]["name"];
 
-    $attore = new Attore();
+    $error_check = validateFields();
 
-    try {
-        $attore->inserisci($nome, $cognome, $data_nascita, $data_morte, $note_carriera);
-        $id_attore = $attore->getLastInsertedAttore()["ID"];
+    if(!$error_check)
+    {
+        $attore = new Attore();
 
-        if (!empty($immagine)) {
-            upload_image(__DIR__."/../../img/attori/", "immagine", $_SESSION["max_dim_img"]);
+        try {
+            $attore->inserisci($nome, $cognome, $data_nascita, $data_morte, $note_carriera);
+            $id_attore = $attore->getLastInsertedAttore()["ID"];
 
-            $percorso_immagine = "attori/". basename($_FILES["immagine"]["name"]);
+            if (!empty($immagine)) {
+                upload_image(__DIR__."/../../img/attori/", "immagine", $_SESSION["max_dim_img"]);
 
-            $immagine = new Immagine();
-            $immagine->inserisci($descrizione_immagine, $percorso_immagine);
+                $percorso_immagine = "attori/". basename($_FILES["immagine"]["name"]);
 
-            $id_immagine = $immagine->getLastInsertedImmagine()["ID"];
+                $immagine = new Immagine();
+                $immagine->inserisci($descrizione_immagine, $percorso_immagine);
 
-            $attore->associa_immagine($id_attore, $id_immagine);
+                $id_immagine = $immagine->getLastInsertedImmagine()["ID"];
+
+                $attore->associa_immagine($id_attore, $id_immagine);
+            }
+
+            header("location: inserisci_attore.php?inserted=1");
+        } catch (Exception $e) {
+            $page = str_replace("#ERRORE_NOME#", " error", $page);
+            $page = str_replace("#ERRORE_NOME#", " error", $page);
+            $esito_inserimento = $e;
         }
-
-        header("location: inserisci_attore.php?inserted=1");
-    } catch (Exception $e) {
-		$page = str_replace("#ERRORE_NOME#", " error", $page);
-        $page = str_replace("#ERRORE_NOME#", " error", $page);
-        $esito_inserimento = $e;
+    }else{
+        $page = str_replace("#NOME_INITIAL#", $nome, $page);
+        $page = str_replace("#COGNOME_INITIAL#", $cognome, $page);
+        $page = str_replace("#DATA_NASCITA_INITIAL#", $data_nascita, $page);
+        $page = str_replace("#DATA_MORTE_INITIAL#", $data_morte, $page);
+        $page = str_replace("#NOTE_CARRIERA_INITIAL#", $note_carriera, $page);
+        $page = str_replace("#DESCRIZIONE_IMMAGINE_INITIAL#", $descrizione_immagine, $page);
     }
+}else{
+    $page = str_replace("#NOME_INITIAL#", "", $page);
+    $page = str_replace("#COGNOME_INITIAL#", "", $page);
+    $page = str_replace("#DATA_NASCITA_INITIAL#", "", $page);
+    $page = str_replace("#DATA_MORTE_INITIAL#", "", $page);
+    $page = str_replace("#NOTE_CARRIERA_INITIAL#", "", $page);
+    $page = str_replace("#DESCRIZIONE_IMMAGINE_INITIAL#", "", $page);
+
+    $page = str_replace("#ERRORE_NOME#", "", $page);
 }
 
-$page = str_replace("#ERRORE_NOME#", "", $page);
-$page = str_replace("#ERRORE_NOME#", "", $page);
 $header = new Header();
 $page = str_replace("<customHeader />", $header->render(), $page);
 $page = str_replace("#ESITO_INSERIMENTO#", $esito_inserimento, $page);
 echo $page;
-?>
+
+function validateFields(){
+    global $page;
+    global $nome;
+    global $cognome;
+    global $data_nascita;
+
+    $attore = new Attore();
+    $error = false;
+
+    if(empty($attore->find_nome_cognome_nascita($nome, $cognome, $data_nascita))) $page = str_replace("#ERRORE_NOME#", "", $page);
+    else { $page = str_replace("#ERRORE_NOME#", " error", $page); $error = true; }
+
+    return $error;
+}
