de239aa3c069844d6ddd7a1223343be446244e3d
Andrea <foxus20@gmail.com>
2022-02-01 21:44:14 +0100

    Aggiunto pagina not_found

---
+++ html/pagine_altre/not_found.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once(__DIR__.'/../../html/componenti/header.php');
+
+$page = file_get_contents(__DIR__.'/not_found.html');
+
+$header = new Header();
+$page = str_replace("<customHeader />", $header->render(), $page);
+echo $page;
--- php/logic/error_reporting.php
+++ php/logic/error_reporting.php
@@ -1,3 +1,4 @@
 <?php
-//TODO: decommentare
-//error_reporting(E_ERROR | E_PARSE);
+require_once(__DIR__."/functions.php");
+
+if(!isStageWebsite()) error_reporting(E_ERROR | E_PARSE);
