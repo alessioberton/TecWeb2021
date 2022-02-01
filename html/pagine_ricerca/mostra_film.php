5f52e81842f3619f3c4afb8d7a351419dee8afa6
avolpe1998 <foxus20@gmail.com>
2022-02-01 10:41:31 +0100

    Fixato warning mostra film

--- html/pagine_ricerca/mostra_film.php
+++ html/pagine_ricerca/mostra_film.php
@@ -124,7 +124,7 @@ if (isset($_GET["titolo"])) {
                             } else{
                                 $page = str_replace("#SALVATO#", "", $page);
                             }
-							 $page = str_replace("#SELECTED".$valutazione_item["Stelle"]."#", "selected", $page);
+							 $page = str_replace("#SELECTED".!empty($valutazione_item["Stelle"]) ?? $valutazione_item["Stelle"]."#", "selected", $page);
 
 							if(isset($_POST)) inserisciModificaValutazione($scheda_utente, $valutazione, $id_film, $scheda_result, $valutazione_item);
                         }else{
